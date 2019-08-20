5bbef341f7ec3f890ff65426839023d2f868a52e
Arief Hirmanto <ariefhirmanto7@gmail.com>
2019-08-20 13:18:05 +0700

    pull origin

--- src/controller/EventController.ts
+++ src/controller/EventController.ts
@@ -1,51 +1,35 @@
 import {Request, Response} from "express";
-import {getManager} from "typeorm";
+import {getManager, Repository} from "typeorm";
 import {Event} from "../entity/Event";
 
 export class EventController {
-    async eventGetByIdAction(request: Request, response: Response) {
 
-        // get a post repository to perform operations with post
-        const eventRepository = getManager().getRepository(Event);
-    
-        // load a post by a given post id
-        const event = await eventRepository.findOne(request.params.id);
-    
-        // if post was not found return 404 to the client
+    eventRepository: Repository<Event>;
+
+    constructor() {
+        this.eventRepository = getManager().getRepository(Event);
+    }
+
+    async eventGetByIdAction(request: Request, response: Response) {
+        const event = await this.eventRepository.findOne(request.params.id);
+
         if (!event) {
             response.status(404);
             response.end();
             return;
         }
-    
-        // return loaded post
+
         response.send(event);
     }
 
     async eventGetAllAction(request: Request, response: Response) {
-
-        // get a post repository to perform operations with post
-        const eventRepository = getManager().getRepository(Event);
-    
-        // load a post by a given post id
-        const events = await eventRepository.find();
-    
-        // return loaded posts
+        const events = await this.eventRepository.find();
         response.send(events);
     }
 
-    eventSaveAction(request: Request, response: Response) {
-
-        // get a post repository to perform operations with post
-        const eventRepository = getManager().getRepository(Event);
-    
-        // create a real post object from post json object sent over http
-        const newEvent = eventRepository.create(request.body);
-    
-        // save received post
-        await eventRepository.save(newEvent);
-    
-        // return saved post back
+    async eventSaveAction(request: Request, response: Response) {
+        const newEvent = this.eventRepository.create(request.body);
+        await this.eventRepository.save(newEvent);
         response.send(newEvent);
     }
 
